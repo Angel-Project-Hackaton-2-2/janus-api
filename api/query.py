9fc748f6115765cd89f0b81585e2bdccabda803a
ssabrut <michael.gunawan2002@gmail.com>
2023-10-08 11:06:31 +0700

    Add user insert prompt scaffolding

--- api/query.py
+++ api/query.py
@@ -32,3 +32,31 @@ async def create_prompt(fingerprint: str):
     )
 
     return {"status": "success", "status_code": 200}
+
+
+@router.post("/api/prompt/{fingerprint}/insert")
+async def insert_prompt(request: Request, fingerprint: str):
+    db = client.get_db()
+    prompt_collections = db["prompts"]
+
+    data = await request.json()
+
+    if "message" not in data:
+        return HTTPException(status_code=400, detail="Invalid request body")
+
+    message = data["message"]
+
+    if not isinstance(message, str):
+        return HTTPException(status_code=400, detail="Invalid request body")
+
+    prompt = prompt_collections.find_one({"fingerprint": fingerprint})
+
+    if prompt is None:
+        return HTTPException(status_code=400, detail="Prompt does not exist")
+
+    prompt_collections.update_one(
+        {"fingerprint": fingerprint},
+        {"$push": {"messages": {"role": "user", "content": message}}},
+    )
+
+    return {"status": "success", "status_code": 200}
